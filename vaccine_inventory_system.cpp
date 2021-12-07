87c09a2f52be9570b991157ba4fb968c325071ae
Tiffany Adora <66481809+tiffanyadora@users.noreply.github.com>
2021-12-07 17:09:34 +0700

    Update vaccine_inventory_system.cpp
    add functions: to add new vaccine, display available vaccines, and check the quantity of a specific vaccine.

--- vaccine_inventory_system.cpp
+++ vaccine_inventory_system.cpp
@@ -5,45 +5,148 @@
 
 using namespace std;
 
-// Global variable
-int vacID, vacCount, vacGap, option;
+class vaccineInventory{
+    private:
+        struct Node
+        {
+            int code;
+            string name;
+            int quantity;
+            int gap;
+            struct Node *next;
+        };
+    public:
+        int vacID, vacCount, vacGap, option;
+        Node *front = NULL,*rear,*temp;
+        void menu();
+        void add_new_vaccine();
+        void delete_vaccine();
+        void display_vaccine();
+        void search_vaccine();
+        void update_vaccine();
+        void check_vaccine_quantity();
+};
 
-/* Link list node */
-struct Node
+void vaccineInventory::add_new_vaccine()
 {
-    int code;
-	string name;
-	int quantity;
-	int gap;
-	struct Node *next;
+	temp = new Node;
+	string inputName;
+	cout<<" Enter vaccine code\t\t: ";
+    cin>>vacID;
+    cin.ignore(1,'\n');
+    cout<<" Enter name of the vaccine\t: ";
+    getline(cin,inputName);
+    cout<<" Enter Initial Quantity\t\t: ";
+    cin>>vacCount;
+    cout<<" Enter the Gap between 2 Doses\t: ";
+    cin>>vacGap;
+	temp->code=vacID;
+	temp->name=inputName;
+	temp->quantity=vacCount;
+	temp->gap=vacGap;
+	temp->next=NULL;
 
-}*front = NULL,*rear,*temp;
-
-void add_new_vaccine(){
+	if(front==NULL)
+		front=rear=temp;
+	else
+	{
+		rear->next=temp;
+		rear=temp;
+	}
+	cout<<"\nNew Vaccine Added Successfully!";
+	getch();
 }
 
-void delete_first_vaccine(){
-}
-
-void display_vaccine(){
-}
-
-void search_vaccine(){
-}
-
-void void update_vaccine(){
-}
-
-void check_vaccine_quantity(){
-}
-
-int main()
+void vaccineInventory::delete_vaccine()
 {
-	int option,vacID,vacCount;
+	/*if(front==NULL)
+		{cout<<" Data of vaccine is not found.\n";
+		getch();}
+	else
+	{
+
+		temp=front;
+		front=front->next;
+		cout<<" Vaccine with "<<temp->code<<" and its data are deleted successfully!\n";
+		delete(temp);
+		getch();
+	}
+	*/
+}
+
+void vaccineInventory::display_vaccine()
+{
+	if(front==NULL)
+		{cout<<" There are no data of vaccine currently.\n";
+		getch();}
+	else
+	{
+		temp=front;
+		cout<<"\n List of Available Vaccines\n";
+		cout<<" +------------------------+-----------------------+-------------------------------+\n";
+		//cout<<setw()
+		cout<<" |\tVaccine name\t  |\tVaccine code\t  |\t  Available quantity\t  |\n";
+		cout<<" +------------------------+-----------------------+-------------------------------+\n";
+		while(temp!=NULL)
+		{
+        cout << " |" << setw(20) << temp->name << setw(5)
+             << "|" << setw(19) << temp->code << setw(5)
+             << "|" << setw(20) << temp->quantity << setw(12)
+             << "|" << endl;
+
+			temp=temp->next;
+		}
+		cout<<" +--------------------------------------------------------------------------------+"<<endl;
+		getch();
+	}
+}
+
+void vaccineInventory::search_vaccine()
+{
+	
+}
+
+void vaccineInventory::update_vaccine()
+{
+	
+}
+
+void vaccineInventory::check_vaccine_quantity()
+{
+    Node *temp;
+    temp=front;
+    int is_found=0;
+    if(front != NULL)
+    {
+        cout<<"\n Enter vaccine code to be checked\t: ";
+        cin>>vacID;
+	    
+        while(temp!=NULL)
+        {
+            temp->code;
+            if(vacID==temp->code)
+            {
+                cout<<" Total number of Vaccine ["<<temp->code<< "]\t\t: "<<temp->quantity;
+                is_found=1;
+            }
+            temp=temp->next;
+        }
+	    
+        if(is_found==0)
+        {
+            cout<<" Data of vaccine is not found.";
+        }
+    }
+    else cout<<" There are no data of vaccine currently.";
+    getch();
+}
+
+void vaccineInventory::menu(){
+    int option,vacID,vacCount;
 	while(1)
 	{
 	    system("cls");
-	    cout<<"\n=================================================";
+	    cout<<"=================================================";
 	    cout<<"\n            Vaccine Inventory System             ";
 		cout<<"\n=================================================\n";
 		cout<<"\n                    Main Menu                    ";
@@ -53,8 +156,8 @@ int main()
 		cout<<"\n 4. Show Vaccine List";
 		cout<<"\n 5. Search Vaccine";
 		cout<<"\n 6. Delete Vaccine";
-		cout<<"\n 7. Keluar";
-		cout<<"\n\n Masukkan Pilihan (1-4): ";
+		cout<<"\n 7. Exit";
+		cout<<"\n\n Enter your choice (1-7): ";
 		cin>>option;
 		cout<<"\n=================================================";
 		cout<<"\n";
@@ -76,15 +179,20 @@ int main()
                 search_vaccine();
                 break;
             case 6:
-			    delete_first_vaccine();
+			    delete_vaccine();
 			    break;
 			case 7:
 			    exit(0);
                 break;
 			default:
-			    cout<<"Invalid input, please enter again. (press any key)";
+			    cout<<"Invalid input, please try again. (press any key)";
 			    getch();
 		}
 	}
-	return 0;
+}
+
+int main()
+{
+    vaccineInventory inventory1;
+    inventory1.menu();
 }
