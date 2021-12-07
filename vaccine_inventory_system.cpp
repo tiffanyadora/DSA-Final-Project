cabfe00672cac01046df98727c50eb8b4d812d71
Tiffany Adora <66481809+tiffanyadora@users.noreply.github.com>
2021-12-07 20:13:15 +0700

    Update vaccine_inventory_system.cpp
    All functions are done.

--- vaccine_inventory_system.cpp
+++ vaccine_inventory_system.cpp
@@ -5,8 +5,10 @@
 
 using namespace std;
 
+/* Class vaccineInventory */
 class vaccineInventory{
     private:
+        // Linked list for Vaccine Data
         struct Node
         {
             int code;
@@ -15,18 +17,23 @@ class vaccineInventory{
             int gap;
             struct Node *next;
         };
+
     public:
-        int vacID, vacCount, vacGap, option;
+        int vacID, vacCount, vacGap, option; //global variable
+
         Node *front = NULL,*rear,*temp;
+
+        // All features of Vaccine Inventory Program
         void menu();
         void add_new_vaccine();
         void delete_vaccine();
         void display_vaccine();
         void search_vaccine();
         void update_vaccine();
-        void check_vaccine_quantity();
+        void check_vaccine_status();
 };
 
+/* Function to insert new vaccine to the inventory */
 void vaccineInventory::add_new_vaccine()
 {
 	temp = new Node;
@@ -40,6 +47,7 @@ void vaccineInventory::add_new_vaccine()
     cin>>vacCount;
     cout<<" Enter the Gap between 2 Doses\t: ";
     cin>>vacGap;
+
 	temp->code=vacID;
 	temp->name=inputName;
 	temp->quantity=vacCount;
@@ -57,23 +65,56 @@ void vaccineInventory::add_new_vaccine()
 	getch();
 }
 
+/* Function to remove vaccine from the inventory */
 void vaccineInventory::delete_vaccine()
 {
-	/*if(front==NULL)
-		{cout<<" Data of vaccine is not found.\n";
-		getch();}
-	else
-	{
+	Node *temp;
+    temp=front;
+    int currID;
+    int is_found=0;
+    if(front != NULL)
+    {
+        cout<<"\n Enter vaccine code to be deleted\t: ";
+        cin>>currID;
 
-		temp=front;
-		front=front->next;
-		cout<<" Vaccine with "<<temp->code<<" and its data are deleted successfully!\n";
-		delete(temp);
-		getch();
-	}
-	*/
+        if(currID==temp->code)
+        {
+            Node *temp=front;
+            front=front->next;
+            delete temp;
+            cout<<"\n Vaccine deleted successfully.";
+            is_found++;
+        }
+
+        else
+        {
+            Node *ptr = front;
+            Node *temp = front;
+            while (temp != NULL)
+            {
+                if(currID == temp->code){
+                    ptr -> next = temp -> next;
+                    delete temp;
+                    cout<<"\n Vaccine deleted successfully.";
+                    is_found++;
+                    break;
+                }
+                ptr = temp;
+                temp = temp-> next;
+            }
+        }
+
+        if(is_found==0)
+        {
+        cout<<" Data of vaccine is not found.";
+        }
+    }
+
+    else cout<<" There are no data of vaccine currently.\n";
+    getch();
 }
 
+/* Function to show all available vaccines in the inventory */
 void vaccineInventory::display_vaccine()
 {
 	if(front==NULL)
@@ -82,9 +123,8 @@ void vaccineInventory::display_vaccine()
 	else
 	{
 		temp=front;
-		cout<<"\n List of Available Vaccines\n";
+		cout<<"\n Table of Available Vaccines\n";
 		cout<<" +------------------------+-----------------------+-------------------------------+\n";
-		//cout<<setw()
 		cout<<" |\tVaccine name\t  |\tVaccine code\t  |\t  Available quantity\t  |\n";
 		cout<<" +------------------------+-----------------------+-------------------------------+\n";
 		while(temp!=NULL)
@@ -101,37 +141,32 @@ void vaccineInventory::display_vaccine()
 	}
 }
 
+/* Function to search the information of vaccine by the code*/
 void vaccineInventory::search_vaccine()
-{
-	
-}
-
-void vaccineInventory::update_vaccine()
-{
-	
-}
-
-void vaccineInventory::check_vaccine_quantity()
 {
     Node *temp;
     temp=front;
+    int currID;
     int is_found=0;
     if(front != NULL)
     {
-        cout<<"\n Enter vaccine code to be checked\t: ";
-        cin>>vacID;
-	    
+        cout<<"\n Enter vaccine code to be searched\t: ";
+        cin>>currID;
         while(temp!=NULL)
         {
             temp->code;
-            if(vacID==temp->code)
+            if(currID==temp->code)
             {
-                cout<<" Total number of Vaccine ["<<temp->code<< "]\t\t: "<<temp->quantity;
-                is_found=1;
+                cout<<"\n\n>>>  The data of vaccine is found!  <<<"<<endl;
+                cout<<" Vaccine Name\t\t: "<<temp->name<<endl;
+                cout<<" Current Quantity\t: "<<temp->quantity<<endl;
+                cout<<" Gap between Dose\t: "<<temp->gap<<endl;
+                cout<<" ------------------------------------------------"<<endl;
+                cout<<"\n\n";
+              is_found=1;
             }
             temp=temp->next;
         }
-	    
         if(is_found==0)
         {
             cout<<" Data of vaccine is not found.";
@@ -141,6 +176,108 @@ void vaccineInventory::check_vaccine_quantity()
     getch();
 }
 
+/* Function to modify the vaccine's quantity either adding or taking some units*/
+void vaccineInventory::update_vaccine()
+{
+    Node *temp;
+    temp=front;
+    int is_found=0;
+    if(front != NULL)
+    {
+        cout<<"\n Enter vaccine code to be updated\t: ";
+        cin>>vacID;
+        cout<<endl;
+        while(temp!=NULL)
+        {
+            temp->code;
+            if(vacID==temp->code)
+            {
+                tryUpdate:
+                cout<<" 1. Add quantity \n";
+                cout<<" 2. Take quantity \n";
+                cout<<" Enter your choice\t: ";
+                cin>>option;
+
+                if(option==1)
+                {
+                cout<<"\n Initial Quantity\t\t: "<<temp->quantity<<endl;
+                cout<<" How much you want to add?\t: ";
+                cin>>vacCount;
+                temp->quantity=temp->quantity+vacCount;
+                cout<<" Updated Quantity\t\t: "<<temp->quantity<<endl;
+                cout<<" ------------------------------------------------"<<endl;
+                cout<<"\n";
+                is_found=1;
+                }
+
+                else if(option==2)
+                {
+                    cout<<"\n Initial Quantity\t\t: "<<temp->quantity;
+                    tryAgain:
+                    cout<<"\n How much you want to take?\t: ";
+                    cin>>vacCount;
+                    if(temp->quantity<vacCount)
+                    {
+                        cout<<" Sorry, the number of vaccine is not enough.";
+                        goto tryAgain;
+                    }
+                    else
+                    {
+                        temp->quantity=temp->quantity-vacCount;
+                        cout<<" Updated Quantity\t\t: "<<temp->quantity<<endl;
+                        cout<<" ------------------------------------------------"<<endl;
+                        cout<<"\n";
+                    }
+                    is_found=1;
+                }
+                else {cout<<"Invalid Input!\n"; goto tryUpdate;}
+            }
+            temp=temp->next;
+        }
+        if(is_found==0)
+        {
+            cout<<" Data of vaccine is not found.";
+        }
+    }
+    else cout<<" There are no data of vaccine currently.";
+    getch();
+}
+
+/* Function to check the availability status of a specific vaccine*/
+void vaccineInventory::check_vaccine_status()
+{
+    Node *temp;
+    temp=front;
+    int is_found=0;
+    if(front != NULL)
+    {
+        cout<<"\n Enter vaccine code to be checked\t: ";
+        cin>>vacID;
+        while(temp!=NULL)
+        {
+            temp->code;
+            if(vacID==temp->code)
+            {
+                if(temp->quantity<=0){
+                    cout<< "\n Vaccine CODE [" <<temp->code<< "] needs to be restocked!";
+                }
+                else{
+                    cout<< "\n Vaccine CODE [" <<temp->code<< "] is available. "<<temp->quantity<<" units left.";
+                }
+                is_found=1;
+            }
+            temp=temp->next;
+        }
+        if(is_found==0)
+        {
+            cout<<" Data of vaccine is not found.";
+        }
+    }
+    else cout<<" There are no data of vaccine currently.";
+    getch();
+}
+
+/* Program menu */
 void vaccineInventory::menu(){
     int option,vacID,vacCount;
 	while(1)
@@ -152,15 +289,17 @@ void vaccineInventory::menu(){
 		cout<<"\n                    Main Menu                    ";
 		cout<<"\n 1. Add New Vaccine";
 		cout<<"\n 2. Update Vaccine Quantity";
-		cout<<"\n 3. Check Vaccine Quantity";
+		cout<<"\n 3. Check Vaccine Status";
 		cout<<"\n 4. Show Vaccine List";
 		cout<<"\n 5. Search Vaccine";
 		cout<<"\n 6. Delete Vaccine";
 		cout<<"\n 7. Exit";
+
 		cout<<"\n\n Enter your choice (1-7): ";
 		cin>>option;
 		cout<<"\n=================================================";
 		cout<<"\n";
+
 		switch(option)
 		{
 			case 1:
@@ -170,7 +309,7 @@ void vaccineInventory::menu(){
                 update_vaccine();
                 break;
             case 3:
-                check_vaccine_quantity();
+                check_vaccine_status();
                 break;
 			case 4:
 			    display_vaccine();
@@ -191,6 +330,7 @@ void vaccineInventory::menu(){
 	}
 }
 
+/* Main function */
 int main()
 {
     vaccineInventory inventory1;
